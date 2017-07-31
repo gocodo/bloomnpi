e743a5d78b3f964822df872b718e03a1eb9ddf4b
Michael M. Wasser <untoldone@users.noreply.github.com>
2017-07-31 15:49:30 -0700

    quick fix to update for new bloomapi ES indexes

--- npi/search_index.go
+++ npi/search_index.go
@@ -120,7 +120,7 @@ func SearchIndex() {
 			fmt.Println(deleteCount, "Records Deleted in", time.Now().Sub(startTime))
 		}
 
-		indexer.Delete("source", "usgov.hhs.npi", id, false)
+		indexer.Delete("usgov.hhs.npi", "main", id, false)
 	}
 
 	indexer.Flush()
@@ -155,7 +155,7 @@ func SearchIndex() {
 			fmt.Println(indexCount, "Records Indexed in", time.Now().Sub(startTime))
 		}
 
-		indexer.Index("source", "usgov.hhs.npi", id, "", nil, doc, false)
+		indexer.Index("usgov.hhs.npi", "main", id, "", nil, doc, false)
 	}
 
 	indexer.Flush()
